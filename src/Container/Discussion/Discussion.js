97e027e977e7ace1a07e139b27dc981ec4960f38
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-08-16 16:38:24 +0430

    Error managment

--- src/Container/Discussion/Discussion.js
+++ src/Container/Discussion/Discussion.js
@@ -7,36 +7,51 @@ import axios from "axios";
 
 const Discussion = () => {
   const [comments, setComemnts] = useState(null);
-  const [selectedId,setSelectedId]= useState(null);
+  const [selectedId, setSelectedId] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const getComments = async () => {
-      const { data } = await axios.get(
-        "http://localhost:3001/comments"
-      );
-      setComemnts(data);
+      try {
+        const { data } = await axios.get("http://localhost:3001/comments");
+        setComemnts(data);
+      } catch (err) {
+        setError(true);
+      }
     };
     getComments();
   }, []);
 
-  const selectedCommentHandler=(id)=>{
+  const selectedCommentHandler = (id) => {
     setSelectedId(id);
-  }
+  };
+
+  const renderComment = () => {
+    let renderValue = <p>Loading...</p>;
+
+    if (error) renderValue = <p>fetching dtata failed</p>;
+
+    if (comments && !error) {
+      renderValue = comments.map((c) => (
+        <Comment
+          key={c.id}
+          name={c.name}
+          email={c.email}
+          onClick={() => selectedCommentHandler(c.id)}
+        />
+      ));
+    }
+    return renderValue;
+  };
 
   return (
     <main>
+      <section>{renderComment()}</section>
       <section>
-        {comments
-          ? comments.map((c) => (
-              <Comment key={c.id} name={c.name} email={c.email} onClick={()=>selectedCommentHandler(c.id)}/>
-            ))
-          : "Loading..."}
+        <FullComment commentId={selectedId} />
       </section>
       <section>
-        <FullComment commentId={selectedId}/>
-      </section>
-      <section>
-        <NewComment setComemnts={setComemnts}/>
+        <NewComment setComemnts={setComemnts} />
       </section>
     </main>
   );
